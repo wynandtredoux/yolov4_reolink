16a51ffcc8a73f2224494a25b1381a35980f3e80
Wynand Tredoux <wynand.tredoux@ucalgary.ca>
2021-12-19 16:03:05 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,17 @@ from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 from pathlib import Path
 import os
 
+# find all files is dir and it's subdirectories ending in .ext
+# dir = folder path as a string: "\path\to\dir"
+# ext = file extension as a string (including the dot): ".mp4"
+def findFiles(dir, ext):
+    found = []
+    for path, subdirs, files in os.walk(dir):  # walk through all files
+        for name in files:
+            if os.path.splitext(name)[1] == ext:  # check if extension matches
+                found.append(os.path.join(path, name))
+    return found
+
 if __name__ == '__main__':
     # parse command line arguments
     parser = ArgumentParser(description='find moving objects of interest in .mp4 files using YOLOv4',
@@ -12,7 +23,7 @@ if __name__ == '__main__':
     print(f'Input file path: {args.input}')
 
     # find all mp4 files
-    for path, subdirs, files in os.walk(args.input):
-        for name in files:
-            print(os.path.join(path, name))
-            print(os.path.join(path, name))
+    mp4s = findFiles(args.input, '.mp4')
+    print(mp4s)
+
+
