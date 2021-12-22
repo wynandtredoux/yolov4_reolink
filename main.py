9a0da914c2381179c5830e3a7ff791ece1a8c81d
Wynand Tredoux <wynand.tredoux@ucalgary.ca>
2021-12-22 15:31:43 -0700

    can now suspend a process while this script needs to use the GPU and resume the process when finished
    everything is working up until the YOLOv4 stuff
    
    all mp4 files are found and converted to a lower fps by ffmpeg
    
    support added for suspending a process while ffmpeg and YOLO use the GPU

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Includes ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+from argparse import ArgumentParser
 from pathlib import Path
 import os
 import ffmpeg
 import sys
 import time
 import datetime
+import psutil
+import cv2
+
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Functions ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -32,13 +35,42 @@ def findFiles(dir, ext, exclude=None):
     return found
 
 
+# pause all processes given by list of pids
+def suspendPid(pids):
+    if pids is None:
+        return
+    for pid in pids:
+        print(f'suspending PID={pid}')
+        try:
+            psutil.Process(pid).suspend()
+        except psutil.Error as e:
+            print(f'error when trying to suspend PID={pid}')
+            print(e, file=sys.stderr)
+            sys.exit(1)
+    return
+
+
+# resume all processes given by list of pids
+def resumePid(pids):
+    if pids is None:
+        return
+    for pid in pids:
+        print(f'resuming PID={pid}')
+        try:
+            psutil.Process(pid).resume()
+        except psutil.Error as e:
+            print(f'error when trying to resume PID={pid}')
+            print(e, file=sys.stderr)
+            sys.exit(1)
+    return
+
 # write a list to a file
 # file = file object from open()
 # list = any python list
-def writeList(file, list):
-    for i in list:
-        file.write(f'{i}\n')
-    return
+# def writeList(file, list):
+#    for i in list:
+#        file.write(f'{i}\n')
+#    return
 
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Start ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -48,17 +80,33 @@ if __name__ == '__main__':
     ffmpeg_err_name = 'ffmpeg_errors.txt'  # text file that contains any errors encountered by ffmpeg
     ext = '.mp4'  # extension of video files
     fps = 10  # framerate that video files should be reduced to
+    # paused = False  # keep track of whether process given in --pid has been paused
 
     # parse command line arguments
-    parser = ArgumentParser(description='find moving objects of interest in .mp4 files using YOLOv4'
-                            , formatter_class=ArgumentDefaultsHelpFormatter)
-    parser.add_argument('darknet_location', help='path for the darknet executable')
-    parser.add_argument('-i', '--input', nargs='?', default='./', type=Path,
-                        help='parent directory of video files')
+    parser = ArgumentParser(description=f'find moving objects of interest in {ext} files using YOLOv4')
+    parser.add_argument('-i', '--input', default='./', type=Path,
+                        help='parent directory of video files. Default is "./"')
+    parser.add_argument('-g', '--gpu', default=0, type=int,
+                        help='Specify which GPU to use in multi-gpu systems. Default is 0')
+    parser.add_argument('-d', '--darknet_location', default='./darknet', type=Path,
+                        help='path for the darknet folder. Default is "./darknet"')
+    parser.add_argument('-p', '--pid', nargs='+', default=None, type=int,
+                        help='specify the PID of 1 or more processes that should to be paused. This may require elevated privileges.')
     args = parser.parse_args()
-    print(f'Input file path: {args.input}')
 
-    mp4s_converted = []  # empty list for storing filepaths for .mp4 files that have been converted
+    # print all args for debugging
+    print(f'Input file path: {args.input}')
+    print(f'GPU ID: {args.gpu}')
+    print(f'darknet folder: {args.darknet_location}')
+    print(f'PID(s): {args.pid}\n')
+
+    # import darknet library
+    sys.path.insert(0, str(args.darknet_location))  # temporarily add darknet folder to path
+    os.add_dll_directory(str(args.darknet_location))  # add darknet folder to dll search path
+    os.add_dll_directory(str(Path(os.getenv('CUDA_PATH')) / 'bin'))  # add cuda bin folder to dll search path
+    import darknet
+
+    mp4s_converted = []  # empty list for storing filepaths for .mp4 files that have been converted already
     # read working file if it exists
     if os.path.isfile(wf_name):
         # read working file
@@ -69,22 +117,40 @@ if __name__ == '__main__':
     # find all video files excluding the ones already in the working file
     mp4s = findFiles(args.input, ext, mp4s_converted)
 
+    # pause all processes given by -p argument
+    suspendPid(args.pid)
+
     # convert video files with ffmpeg to reduce framerate (uses NVENC for fast encoding)
     for i in mp4s:
         output_path = os.path.splitext(i)[0] + '.lowfps'  # same filename but different extension
         stream = ffmpeg.input(i).filter('fps', fps=fps) \
-            .output(output_path, f='mp4', vcodec='h264_nvenc', crf=23) \
-            .overwrite_output().global_args('-an') # setup ffmpeg stream to convert video to framerate set by fps variable and remove audio since we don't need it
+            .output(output_path, f='mp4', vcodec='h264_nvenc', crf=23, gpu=args.gpu) \
+            .overwrite_output().global_args('-an')  # setup ffmpeg stream to convert video to framerate set by fps variable and remove audio since we don't need it
+        #failed = False
         try:  # try ffmpeg conversion
             out, err = stream.run(capture_stdout=True, capture_stderr=True)
         except ffmpeg.Error as e:  # if an error occurs, print error to console and ffmpeg_err_name file and continue with the next video
+            #failed = True
             err = e.stderr.decode()
             print(err, file=sys.stderr)
             with open(ffmpeg_err_name, 'a') as file:
                 sttime = datetime.datetime.fromtimestamp(time.time()).strftime('[%d_%m_%Y_%H:%M:%S]')  # create timestamp
                 file.write(f'~~~~~~~~~~~~~~~~~~~~~~{sttime}~~~~~~~~~~~~~~~~~~~~~~\n{err}\n')
-        # after ffmpeg is done re-encoding the file (or failed), add file path to working file so it doesn't get converted again
-        with open(wf_name, 'a') as file:
-            file.write(f'{i}\n')
+        finally:
+            # after ffmpeg is done re-encoding the file (or failed), add file path to working file so it doesn't get converted again
+            with open(wf_name, 'a') as file:
+                file.write(f'{i}\n')
 
+    # TODO:
     # send converted files to darknet YOLOv4
+        # check if file.lowfps exists
+
+            # find objects with YOLO
+
+            # determine if object is stationary
+
+            # remove file from working.txt after it has been processed by YOLO
+
+    # resume paused processes
+    resumePid(args.pid)
+
